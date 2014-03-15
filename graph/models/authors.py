89cc1d8010bb1d8cf3e5e36c89f6c13e64f70ae6
Efe Karakus <efekarakus@gmail.com>
2014-03-15 00:01:54 -0500

    removing print statements

--- graph/models/authors.py
+++ graph/models/authors.py
@@ -10,20 +10,14 @@ class Authors(object):
         Clears the nodes with label Author.
         Reads authors.txt and adds them into the graph database.
         """
-        print "# AUTHORS"
-        print "## CLEARING AUTHORS"
         Authors.clear()
-        print "## DONE CLEARING AUTHORS"
-        print "## ADDING AUTHORS"
         batch = neo4j.WriteBatch(newsnet_db)
         with open(Authors.DATA_PATH, 'r') as f:
             for line in f:
-                print line.strip()
                 [aid, aname] = line.strip().split('\t')
                 author = batch.create(node({'name': aname}))
                 batch.add_labels(author, "Author")
         batch.submit()
-        print "## DONE ADDING AUTHORS"
 
     @staticmethod
     def clear():
