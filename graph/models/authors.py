05276f081bda9bac1d41be7da097cf4d6fbff334
Efe Karakus <efekarakus@gmail.com>
2014-03-15 10:56:58 -0500

    works but hella slow

--- graph/models/authors.py
+++ graph/models/authors.py
@@ -10,19 +10,43 @@ class Authors(object):
         Clears the nodes with label Author.
         Reads authors.txt and adds them into the graph database.
         """
+        logger.info("AUTHORS: starting clear")
         Authors.clear()
+        logger.info("AUTHORS: end clear")
         batch = neo4j.WriteBatch(newsnet_db)
+
+        count = 0
+        num_batch = 0
         with open(Authors.DATA_PATH, 'r') as f:
             for line in f:
-                [aid, aname] = line.strip().split('\t')
-                author = batch.create(node({'name': aname}))
+                vals = line.strip().split('\t')
+                if len(vals) <= 1: continue
+
+                aid, aname = vals[0], vals[1]
+                author = batch.create(node({'id': aid, 'name': aname}))
                 batch.add_labels(author, "Author")
-        batch.submit()
+                count += 1
+
+                # submit every BATCH_SIZE
+                if count == BATCH_SIZE:
+                    batch.run()
+                    num_batch += 1
+                    logger.info("AUTHORS: submitted batch [%d, %d]" % ((num_batch-1)*BATCH_SIZE, num_batch*BATCH_SIZE))
+                    count = 0
+
+        if count > 0:
+            batch.run()
+        logger.info("AUTHORS: done")
 
     @staticmethod
     def clear():
         nodes = newsnet_db.find("Author")
         batch = neo4j.WriteBatch(newsnet_db)
+        count = 0
         for n in nodes:
             batch.delete(n)
-        batch.submit()
+            count += 1
+            if count == BATCH_SIZE:
+                batch.run()
+        if count > 0:
+            batch.run()
